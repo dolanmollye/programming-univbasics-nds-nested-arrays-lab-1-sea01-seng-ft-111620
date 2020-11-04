8dbf6a2602f3479a298f489c8e02f2db2e86f06f
Molly Dolan <dolanmollye@gmail.com>
2020-11-04 03:48:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,6 +37,7 @@ end
 
 
 def matrix_update(matrix, row, column, new_value)
+  matrix[0][2]
   matrix[0][2] = "Lemons"
   return matrix
   # Given any matrix (array of arrays), a row index and a column index, 
